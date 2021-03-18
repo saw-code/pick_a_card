212037f092836b88f5eaa26321837ad89bfbeace
saw <rus_xakep@mail.ru>
2021-03-18 12:12:11 +0300

    Alternative output & Joker

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 puts "Pick a Card"
+
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 suits = %w[♠ ♥ ♦ ♣]
 
@@ -9,6 +10,10 @@ values.each do |value|
   suits.each do |suit|
     cards << "#{value} of #{suit}"
   end
+end
+
+2.times do
+  cards << '[Joker]'
 end
 
 cards.shuffle!
